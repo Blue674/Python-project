82500c49df7b93cc5c3bedcedacd1e487f90bab6
Lalitha S <104846703+Blue674@users.noreply.github.com>
2022-07-04 23:54:37 +0530

    Update 1.Write_og_list.py

--- 1.Write_og_list.py
+++ 1.Write_og_list.py
@@ -2,9 +2,9 @@ import pickle
   
 file = open("ProductList.txt", "wb")
   
-my_dict = {"CRMS": ['Strawberry Ice Cream',35.0,1.0],
-           "CRMC": ['Chocolate Ice Cream',45.0,1.0],
-           "CRMV": ['Vanilla Ice Cream',25.0,1.0],
+my_dict = {"CRMS": ['Str ICream',35.0,1.0],
+           "CRMC": ['Choco ICream',45.0,1.0],
+           "CRMV": ['Vnll ICream',25.0,1.0],
            "PCHP": ['Potato Chips',57.0,1.0],
            "AT":['Gold Wheat Atta',62.0,0.5],
            "BRC":['Basamti Rice',74.0,1.0],
